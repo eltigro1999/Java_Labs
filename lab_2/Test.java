c5f7e75f95a416b5bf90023bdccb67eae0246246
eltigro1999 <eltigrolearning@gmail.com>
2021-11-05 00:09:59 +0300

    Lab3 added as lab_31

--- lab_2/Test.java
+++ lab_2/Test.java
@@ -8,7 +8,10 @@ class Test {
 		System.out.println();
 	}
 
-	public static void main(String args[]) {
+	public static void lab2() {
+
+		//Lab 2
+
 		//Vehicle v=new Vehicle();	It's abstract. So the object of the abstract class
 		//				 is not allowed to be created
 		
@@ -17,8 +20,7 @@ class Test {
 		Motorbike suzuki=new Motorbike("Suzuki", "V-STROM", 200, 2, 173);	
 		Polymorphism(suzuki);
 		
-		
-		
+				
 		Car car=new Car();
 		Polymorphism(car);
 		Car audi=new Car("Audi", "q8", 600, 5, 250);
@@ -39,5 +41,12 @@ class Test {
 		Polymorphism(trailer);
 		Trailer krone=new Trailer("Krone", "Model", 10000, 40, 125);
 		Polymorphism(krone);
+		trailer.addTruck(truck);
+		System.out.println("Has truck:"+trailer.HasTruck());
+		System.out.println("Speed:"+trailer.GetMaxSpeed());
+	}
+
+	public static void main(String args[]) {
+		lab2();
 	}
 }
--- lab_2/Vehicle.java
+++ lab_2/Vehicle.java
@@ -1,9 +1,11 @@
 abstract class Vehicle {
-	private String brand;
-	private String model;
-	private int max_cargo_weight;
-	private int passengers_quantity;
-	private int max_speed;
+	protected String brand;
+	protected String model;
+	protected int max_cargo_weight;
+	protected int passengers_quantity;
+	protected int max_speed;
+	protected static int static_id=0;
+	protected int id=static_id;
 	Vehicle() {
 		this("BMW", "X5", 500, 5, 250);
 	}
@@ -13,6 +15,7 @@ abstract class Vehicle {
 		this.max_cargo_weight=max_cargo_weight;
 		this.passengers_quantity=passengers_quantity;
 		this.max_speed=max_speed;
+		static_id+=1;
 	}
 	String GetBrand() {
 		return brand;
@@ -28,5 +31,11 @@ abstract class Vehicle {
 	}
 	int GetMaxSpeed() {
 		return max_speed;
+	}
+	int GetId() {
+		return id;
+	}
+	static int GetStaticId() {
+		return static_id;
 	}
 }
