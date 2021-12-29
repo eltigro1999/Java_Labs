bf31219216ea1fc1d047ca0e2b56e376c29cd7d2
eltigro1999 <eltigrolearning@gmail.com>
2021-12-29 14:44:36 +0300

    Graphics are added Lab 3, 4, 5, 6 are added

--- lab_4/Car.java
+++
@@ -1,8 +0,0 @@
-package lab_4;
-
-public class Car extends Vehicle {
-	Car() {}
-	Car(String brand, String model,int max_cargo_weight,int passengers_quantity, int max_speed) {
-		super(brand, model, max_cargo_weight, passengers_quantity, max_speed);
-	}
-}
